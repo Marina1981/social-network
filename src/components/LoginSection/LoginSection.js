e087e2631e395ede1fd1606ad4afe4035c5eff3c
Marina1981 <marina-sergeevna@inbox.ru>
2019-02-05 21:41:41 +0300

    анимацию закоментировала

--- src/components/LoginSection/LoginSection.js
+++ src/components/LoginSection/LoginSection.js
@@ -9,128 +9,144 @@ const LoginSection = (props) => {
     if (props.isAuth) {
         return <Redirect to="/profile"/>
     }
-   
+
     //---------------------------
     let errorMessageBlock = (props.loginingError !== loginingProcessResults.SUCCESS) ?
         (<div className="error-block">
             <div className="error-message">
                 {props.loginingErrorMessage}
             </div>
-                {(props.loginingError === loginingProcessResults.CAPTCHA_REQUIRED_ERROR) ?
-                    (<div className="captcha-block">
-                            <img className="captcha-block__images" src={props.captchaUrl} alt="captcha"/>
-                            <input className="captcha-block__input" value={props.creatingCaptcha}
-                                   onChange={
-                                       (e) => {
-                                           props.onChangeCreatingCaptcha(e.currentTarget.value)
-                                       }
-                            }/>
-                            <button className="captcha-block__button"
-                                    onClick={
-                                        (e) => {
-                                            props.addCreatingCaptcha()
-                                        }
-                                    }>
-                                add
-                            </button>
-                        </div>
+            {(props.loginingError === loginingProcessResults.CAPTCHA_REQUIRED_ERROR) ?
+                (<div className="captcha-block">
+                        <img className="captcha-block__images" src={props.captchaUrl} alt="captcha"/>
+                        <input className="captcha-block__input" value={props.creatingCaptcha}
+                               onChange={
+                                   (e) => {
+                                       props.onChangeCreatingCaptcha(e.currentTarget.value)
+                                   }
+                               }/>
+                        <button className="captcha-block__button"
+                                onClick={
+                                    (e) => {
+                                        props.addCreatingCaptcha()
+                                    }
+                                }>
+                            add
+                        </button>
+                    </div>
 
-                    ) : null
-                }
+                ) : null
+            }
         </div>) : null;
     //---------------------------
 
     //---------------------------
     return (
-        <>
-            {props.loginingStatus !== loginingProcessStatuses.IN_PROGRESS ?
-                <div className="c-login-section-wrapper">
-                    <div className="c-login-section">
-                        <div className="c-login-section__index-login-form--positioned">
-                            <div className="input-form">
+
+        <div className="c-login-section-wrapper">
+            <div className="c-login-section">
+                <div className="c-login-section__index-login-form--positioned">
+                    <div className="input-form">
                         <span className="input-form__label">
                             Email
                         </span>
-                                <input className="input-form__input" placeholder='email'
-                                       value={props.creatingUserLogin}
-                                       onChange={
-                                           (e) => {
-                                               props.onChangeCreatingLogin(e.currentTarget.value)
-                                           }
-                                       }/>
-                            </div>
-                        </div>
-                        <div className="c-login-section__index-password-form--positioned">
-                            <div className="input-form">
+                        <input className="input-form__input" placeholder='email'
+                               value={props.creatingUserLogin}
+                               onChange={
+                                   (e) => {
+                                       props.onChangeCreatingLogin(e.currentTarget.value)
+                                   }
+                               }/>
+                    </div>
+                </div>
+                <div className="c-login-section__index-password-form--positioned">
+                    <div className="input-form">
                         <span className="input-form__label">
                             Password
                         </span>
-                                <input className="input-form__input" placeholder='password' type="password"
-                                       value={props.creatingUserPassword}
-                                       onChange={
-                                           (e) => {
-                                               props.onChangeCreatingPassword(e.currentTarget.value)
-                                           }
-                                       }/>
-                            </div>
-                        </div>
-                        <div className="c-login-section__button-box">
-                            <input className="button-box__checkbox" type="checkbox"
-                                   onChange={
-                                       (e) => {
-                                           props.onChangeRememberMeFlag(e.currentTarget.checked)
-                                       }
-                                   }/>
-                            <span className="checkbox-label">
+                        <input className="input-form__input" placeholder='password' type="password"
+                               value={props.creatingUserPassword}
+                               onChange={
+                                   (e) => {
+                                       props.onChangeCreatingPassword(e.currentTarget.value)
+                                   }
+                               }/>
+                    </div>
+                </div>
+                <div className="c-login-section__button-box">
+                    <input className="button-box__checkbox" type="checkbox"
+                           onChange={
+                               (e) => {
+                                   props.onChangeRememberMeFlag(e.currentTarget.checked)
+                               }
+                           }/>
+                    <span className="checkbox-label">
                             remember me
                         </span>
-                            {/*------------------------------------------------*/}
-                            <button className="button-box__button" onClick={
-                                (e) => {
-                                    props.onLoginButtonClick();
-                                }}
-                                    disabled={props.loginingStatus === loginingProcessStatuses.IN_PROGRESS}>
-                                Login
-                            </button>
-                        </div>
-                        {/*------------------------------------------------*/}
-                        {errorMessageBlock}
-                        {/*------------------------------------------------*/}
-                        {/*<button className="c-login-section__registration-button">*/}
-                        {/*registration*/}
-                        {/*</button>*/}
-                    </div>
-                    {/*------------------------------------------------*/}
+                    <button className="button-box__button" onClick={
+                        (e) => {
+                            props.onLoginButtonClick();
+                        }}
+                            disabled={props.loginingStatus === loginingProcessStatuses.IN_PROGRESS}>
+                        Login
+                    </button>
+                </div>
+                {/*------------------------------------------------*/}
+                {errorMessageBlock}
+                {/*------------------------------------------------*/}
+                {/*<button className="c-login-section__registration-button">*/}
+                {/*registration*/}
+                {/*</button>*/}
+            </div>
+        </div>
 
-                    {/*------------------------------------------------*/}
-                </div> :
-                <div className="container" aria-busy="true"
-                     aria-label="Loading" role="progressbar">
-                    <div className="swing">
-                        <div className="swing-l"/>
-                        <div/>
-                        <div/>
-                        <div/>
-                        <div/>
-                        <div/>
-                        <div className="swing-r"/>
-                    </div>
-                    <div className="shadow">
-                        <div className="shadow-l"/>
-                        <div/>
-                        <div/>
-                        <div/>
-                        <div/>
-                        <div/>
-                        <div className="shadow-r"/>
-                    </div>
-                </div>}
-        </>
     );
 };
 
 export default LoginSection;
 
 
+{/*<div className="container" aria-busy="true"*/
+}
+{/*aria-label="Loading" role="progressbar">*/
+}
+{/*<div className="swing">*/
+}
+{/*<div className="swing-l"/>*/
+}
+{/*<div/>*/
+}
+{/*<div/>*/
+}
+{/*<div/>*/
+}
+{/*<div/>*/
+}
+{/*<div/>*/
+}
+{/*<div className="swing-r"/>*/
+}
+{/*</div>*/
+}
+{/*<div className="shadow">*/
+}
+{/*<div className="shadow-l"/>*/
+}
+{/*<div/>*/
+}
+{/*<div/>*/
+}
+{/*<div/>*/
+}
+{/*<div/>*/
+}
+{/*<div/>*/
+}
+{/*<div className="shadow-r"/>*/
+}
+{/*</div>*/
+}
+{/*</div>*/
+}
 
 
