ea14f4ad862765d145eb42f82b2d34b2a203ee83
MarinaSMS <17121981mar@gmail.com>
2019-03-04 21:02:48 +0300

    tests

--- src/components/_tests_/auth.test.js
+++ src/components/_tests_/auth.test.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import reducer, {types} from "../../redux/modules/authRedux";
 
+
+
 test('data added successfully', () => {
    let newState = reducer ({
        userAuthData: {
-           userId:    '',
-           userLogin: '',
-           userEmail: ''}
-   }, {type: types.SET_USER_AUTH_DATA, userId: '1', userLogin: 'mahaha',  userEmail: 'ееее'});
-    expect(newState.userAuthData.userId.typeof).toEqual("string");
-    expect(newState.userAuthData.userLogin.typeof).toEqual("string");
-    expect(newState.userAuthData.userEmail.typeof).toEqual("string");
+           userId:    null,
+           userLogin: null,
+           userEmail: null}
+   }, {type: types.SET_USER_AUTH_DATA, userId: '1',userLogin: 'mahaha',  userEmail: 'ееее'});
+    expect(newState.userAuthData.userId).toBe('1');
+    expect(newState.userAuthData.userLogin).toBe('mahaha');
+    expect(newState.userAuthData.userEmail).toBe('ееее');
+});
+
+//------------//
+
+test('data clear successfully', () => {
+    let newState = reducer ({
+        userAuthData: {
+            userId:    '1',
+            userLogin: 'mahaha',
+            userEmail: 'ееее'}
+    }, {type: types.CLEAR_USER_AUTH_DATA, userId: null,userLogin: null,  userEmail: null});
+    expect(newState.userAuthData.userId).toBe(null);
+    expect(newState.userAuthData.userLogin).toBe(null);
+    expect(newState.userAuthData.userEmail).toBe(null);
+});
+
+//------------//
+
+test('avatar successfully added', () => {
+    let newState = reducer ( {
+        avatar: null
+    }, {type: types.SET_USER_AVATAR, url: 'ggggg'});
+    expect(newState.avatar).toBe('ggggg');
 });
